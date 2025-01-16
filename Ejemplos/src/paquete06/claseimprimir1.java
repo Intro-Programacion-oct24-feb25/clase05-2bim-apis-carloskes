975094053b598af6f73ef857ea298a8c5403a0ce
unknown <cscorrea@utpl.edu.ec>
2025-01-16 11:43:22 -0500

    actualizado

---
+++ Ejemplos/src/paquete06/claseimprimir1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete06;
+
+/**
+ *
+ * @author utpl
+ */
+public class claseimprimir1 {
+    public static void claseimprimir1(int d, int j, int valorA, int valorB) {
+        System.out.printf("La suma de %d + %d es igual a: %d\n"
+                + "La multiplicacion de %d * %d es igual a: %d\n",
+                valorA,valorB,
+                d,
+                valorA, valorB,
+                j);
+    }
+    
+}
